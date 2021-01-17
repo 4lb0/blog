6fecdc3a3c02d5ff52d7180b8ce938aeb767a302
Rodrigo Arce <albo@pragmore.com>
2021-01-17 10:54:52 -0300

    Change text in about

--- src/Component/AboutMe.php
+++ src/Component/AboutMe.php
@@ -18,7 +18,7 @@ class AboutMe
                 Hey, my name is Rodrigo but I prefer to be called <strong>Albo</strong> (<ExternalLink href="https://en.wikipedia.org/wiki/All_Boys">All Boys</ExternalLink>' nickname).
                 I've beeing getting paid for developing web apps for the last 15+ years, mostly in PHP and JS.
             </p>
-            <p>I'm a father of 3 boys and a husband.</p>
+            <p>I'm a father of 3 boys and a husband. In 2010 I co-found <ExternalLink href="https://pragmore.com">Pragmore</ExternalLink>.</p>
             <p>
                 This is minimalist blog written with <ExternalLink href="https://github.com/pragmore/fernet">Fernet</ExternalLink> (because <ExternalLink href="https://edume.com/blog/what-is-dogfooding">dogfooding</ExternalLink> is good).
                 You can see the <ExternalLink href="https://github.com/4lb0/blog">full code in GitHub</ExternalLink>.
