b7d2a170f587e69607d8b68f042464b1e224dfe0
Rodrigo Arce <albo@pragmore.com>
2021-10-26 03:37:17 -0300

    Improve dev server

--- src/DevServer.php
+++ src/DevServer.php
@@ -8,22 +8,25 @@ class DevServer
     const TITLE = 'albo.ar';
     const LOGO = '💡';
     const TAGS_PREFIX = 'tag-';
-    const IMAGES_URL = '/images';
 
     public function __invoke()
     {
         $url = pathinfo($_SERVER['REQUEST_URI']);
         $render = new Render();
-        if ($url['filename'] === '') {
+        $staticFile = __DIR__ . '/../public' . $_SERVER['REQUEST_URI'];
+        if (file_exists($staticFile) && is_file($staticFile)) {
+            echo file_get_contents($staticFile);
+        } elseif ($url['filename'] === '') {
             echo $render('home', ['posts' => (new Posts)()]);
-        } elseif ($url['dirname'] === static::IMAGES_URL) {
-            echo file_get_contents(__DIR__ . '/../public' . $_SERVER['REQUEST_URI']);
         } elseif (substr($url['filename'], 0, strlen(static::TAGS_PREFIX)) === static::TAGS_PREFIX) {
             $tag = substr($url['filename'], strlen(static::TAGS_PREFIX));
             $posts = (new Posts)($tag);
             echo $render('tags', ['posts' => $posts, 'tag' => $tag]);
-        } else {
+        } elseif (file_exists(__DIR__ . '/../pages/' . $url['filename'] . '.md')) {
             echo $render->post($url['filename']);
+        } else {
+            header('HTTP/1.0 404 Not Found');
+            echo '<html><body>404 Not Found</body></html>';
         }
     }
 }
