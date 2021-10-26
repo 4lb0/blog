e0182c255b9bca2fd7a7d1401da1f2e4d4fb80de
Rodrigo Arce <albo@pragmore.com>
2021-10-26 03:54:15 -0300

    Change language in alt

--- templates/home.php
+++ templates/home.php
@@ -1,7 +1,7 @@
 <?php include __DIR__ . '/header.php'; ?>
 <main>
     <p class="text-center">
-        <img alt="Photo of Albo" src="profile.webp" width="100" height="100" />
+        <img alt="Foto de Albo" src="profile.webp" width="100" height="100" />
     </p>
     <h1 class="text-center">
         <?= \Blog\DevServer::LOGO ?> <?= \Blog\DevServer::TITLE ?>
