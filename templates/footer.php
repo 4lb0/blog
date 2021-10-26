9135e21c11c2257175f15cbda370852ed5e3a339
Rodrigo Arce <albo@pragmore.com>
2021-10-26 03:49:29 -0300

    Change links in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,16 +1,16 @@
   <footer>
     <span>
-      <a href="https://twitter.com/4lb0" target="_blank" aria-label="Twitter">
+      <a href="https://twitter.com/4lb0" rel="noopener" aria-label="Twitter">
         <i class="tw" aria-hidden="true" title="Twitter"></i>
       </a>
     </span>
     <span>
-      <a href="https://github.com/4lb0" target="_blank" aria-label="GitHub">
+      <a href="https://github.com/4lb0" rel="noopener" aria-label="GitHub">
         <i class="gh" aria-hidden="true" title="GitHub"></i>
       </a>
     </span>
     <span>
-      <a href="https://www.linkedin.com/in/rodrigoarce/" target="_blank" aria-label="LinkedIn">
+      <a href="https://www.linkedin.com/in/rodrigoarce/" rel="noopener" aria-label="LinkedIn">
         <i class="in" aria-hidden="true" title="LinkedIn"></i>
       </a>
     </span>
