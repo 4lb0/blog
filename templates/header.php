4fbc2708bdf086bdd780fe87660afb61b481d918
Rodrigo Arce <albo@pragmore.com>
2021-10-26 03:48:01 -0300

    Fix media in css link

--- templates/header.php
+++ templates/header.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22><?= \Blog\DevServer::LOGO ?></text></svg>">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/4lb0/blouse.css@latest/dist/blouse.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/4lb0/blouse.css@latest/dist/blouse.css" media="screen" crossorigin="anonymous">
   <style>
     @font-face {
       font-family: 'Literata';
