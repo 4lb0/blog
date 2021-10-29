798764f0691032bb84ecd1de894350c7e1cc4b86
Rodrigo Arce <albo@pragmore.com>
2021-10-29 10:40:03 -0300

    Remove author from list

--- templates/_post.php
+++ templates/_post.php
@@ -1,5 +1,5 @@
     <p class="mt-2">
-        Publicado por <a href="."><?= $author ?? getenv('BLOG_AUTHOR') ?></a> el <?= print_date($date) ?>.
+        Publicado el <?= print_date($date) ?>.
         <?php if (count($tags)): ?>
             Tags:
             <?php foreach ($tags as $i => $tag): ?> 
