f00782b6f80a720bf2eb0c7c49042018e97c7a3b
Rodrigo Arce <albo@pragmore.com>
2021-12-15 01:16:48 -0300

    Remove icons

--- templates/footer.php
+++ templates/footer.php
@@ -1,17 +1,17 @@
   <footer>
     <span>
       <a href="https://twitter.com/4lb0" rel="noopener" aria-label="Twitter">
-        <i class="tw" aria-hidden="true" title="Twitter"></i>
+        Twitter
       </a>
     </span>
     <span>
       <a href="https://github.com/4lb0" rel="noopener" aria-label="GitHub">
-        <i class="gh" aria-hidden="true" title="GitHub"></i>
+        Github
       </a>
     </span>
     <span>
       <a href="https://www.linkedin.com/in/rodrigoarce/" rel="noopener" aria-label="LinkedIn">
-        <i class="in" aria-hidden="true" title="LinkedIn"></i>
+        LinkedIn
       </a>
     </span>
   </footer>
