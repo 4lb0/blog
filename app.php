ae0d576c079be96b616004ded4846b9309366f45
Rodrigo Arce <albo@pragmore.com>
2021-12-27 11:19:50 -0300

    Fix local server bug serving images with the same post name

--- app.php
+++ app.php
@@ -11,14 +11,14 @@ $staticFile = __DIR__ . '/public' . $_SERVER['REQUEST_URI'];
 
 if ($url['filename'] === '') {
     echo render('home', ['posts' => Posts::list()]);
+} elseif (file_exists($staticFile) && is_file($staticFile)) {
+    return false;
 } elseif (substr($url['filename'], 0, strlen(BLOG_TAGS_PREFIX)) === BLOG_TAGS_PREFIX) {
     $tag = substr($url['filename'], strlen(BLOG_TAGS_PREFIX));
     echo render('tags', ['posts' => Posts::listByTag($tag), 'tag' => $tag]);
 } elseif (Posts::exists($url['filename'])) {
     $post = Posts::get($url['filename']);
     echo render($post['template'], $post);
-} elseif (file_exists($staticFile) && is_file($staticFile)) {
-    return false;
 } else {
     header('HTTP/1.0 404 Not Found');
     echo '<html><body>404 Not Found</body></html>';
