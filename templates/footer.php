d6bb4be082b5cef8dd585ccf77ea7fcf6cfbb2a0
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-03-03 21:09:49 -0300

    Remove unnecesary spans in footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,13 +1,7 @@
   <footer>
-    <span>
-      <a href="https://twitter.com/4lb0" rel="noopener" aria-label="Twitter">Twitter</a>
-    </span>
-    <span>
-      <a href="https://github.com/4lb0" rel="noopener" aria-label="GitHub">Github</a>
-    </span>
-    <span>
-      <a href="https://www.linkedin.com/in/rodrigoarce/" rel="noopener" aria-label="LinkedIn">LinkedIn</a>
-    </span>
+    <a href="https://twitter.com/4lb0" rel="noopener">Twitter</a>
+    <a href="https://github.com/4lb0" rel="noopener">Github</a>
+    <a href="https://www.linkedin.com/in/rodrigoarce/" rel="noopener">LinkedIn</a>
   </footer>
 <?php /* Close the tags that have been opened in header.php */ ?>
 </body>
