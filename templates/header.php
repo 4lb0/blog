c2be912ffb8ad62bb747e633d1d7f5a46cffde73
Rodrigo “Albo” Arce <albo@pragmore.com>
2022-05-02 11:01:49 -0300

    Reduce favicon size

--- templates/header.php
+++ templates/header.php
@@ -7,7 +7,7 @@
 <?php if(isset($description)): ?>
   <meta name="description" content="<?= $description ?>">
 <?php endif; ?>
-  <link rel="icon" href="data:image/svg+xml,%3Csvg%20xmlns=%22http://www.w3.org/2000/svg%22%20viewBox=%220%200%20100%20100%22%3E%3Ctext%20y=%22.9em%22%20font-size=%2290%22%3E<?= getenv('BLOG_LOGO') ?: BLOG_DEFAULT_LOGO ?>%3C/text%3E%3C/svg%3E">
+  <link rel="icon" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'><text y='14' x='0'><?= getenv('BLOG_LOGO') ?: BLOG_DEFAULT_LOGO ?></text></svg>">
   <style><?php echo file_get_contents(__DIR__ . '/../src/u.css') ?><?= $style ?? '' ?></style>
 </head>
 <body>
