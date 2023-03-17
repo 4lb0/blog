557fd5304305e8e0a453d7e95c487cd3becec721
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-03-17 15:06:59 -0300

    Refactor

--- app.php
+++ app.php
@@ -4,18 +4,18 @@ require_once 'common.php';
 
 use Blog\Posts;
 
-$url = pathinfo($_SERVER['REQUEST_URI']);
+$url = pathinfo($_SERVER['REQUEST_URI'])['filename'];
 $staticFile = __DIR__ . '/public' . $_SERVER['REQUEST_URI'];
 
-if ($url['filename'] === '') {
+if ($url === '') {
     echo render('home', ['posts' => Posts::list()]);
 } elseif (file_exists($staticFile) && is_file($staticFile)) {
     return false;
-} elseif (substr($url['filename'], 0, strlen(BLOG_TAGS_PREFIX)) === BLOG_TAGS_PREFIX) {
-    $tag = substr($url['filename'], strlen(BLOG_TAGS_PREFIX));
+} elseif (substr($url, 0, strlen(BLOG_TAGS_PREFIX)) === BLOG_TAGS_PREFIX) {
+    $tag = substr($url, strlen(BLOG_TAGS_PREFIX));
     echo render('tags', ['posts' => Posts::listByTag($tag), 'tag' => $tag]);
-} elseif (Posts::exists($url['filename'])) {
-    $post = Posts::get($url['filename']);
+} elseif (Posts::exists($url)) {
+    $post = Posts::get($url);
     echo render($post['template'], $post);
 } else {
     header('HTTP/1.0 404 Not Found');
