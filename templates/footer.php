eb9a7570fc938da725bd2b731b177212ec7164b8
Rodrigo “Albo” Arce <albo@pragmore.com>
2023-04-15 13:09:36 -0300

    Update footer

--- templates/footer.php
+++ templates/footer.php
@@ -3,8 +3,8 @@
         <ul>
             <li><a href="."><?= $_ENV['BLOG_TITLE'] ?></a></li>
             <li><a href="//twitter.com/4lb0">Twitter</a>
+            <li><a href="//twitch.tv/albo_ar">Twitch</a>
             <li><a href="//github.com/4lb0">Github</a>
-            <li><a href="//www.linkedin.com/in/rodrigoarce/">LinkedIn</a>
             <li><a href="#" aria-label="Volver a leer"><span aria-hidden="true">↑ </span>Arriba</a></li>
         </ul>
     </nav>
